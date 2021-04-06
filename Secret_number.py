8417d9e9cbc9539e2cebced2e06058dd51033774
alexs2307 <sold.alex@gmail.com>
2021-04-06 16:15:09 +0200

    5-naloga

---
+++ Secret_number.py
@@ -0,0 +1,7 @@
+secret = str(5)
+guess = input("Guess the secret number between 1 and 10 ")
+
+if secret == guess:
+    print("Congratulations the secret number is 5!! ")
+else:
+    print("Oh no!! Better luck next time")
