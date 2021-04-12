d80b1b1f43d6406ec829fac818b277e2d0a9eb0b
alexs2307 <sold.alex@gmail.com>
2021-04-12 22:10:47 +0200

    naloga6

---
+++ Naloga_6/string_lowercase_letters.py
@@ -0,0 +1,7 @@
+print("convert string in to lowercase letters")
+
+string = input("")
+if string != string.lower():
+    print(string.lower())
+else:
+    print("your string has already lowercase letters")
