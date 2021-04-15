079484c499c24d8d9ac6fb78e20aed6ad175a5a5
alexs2307 <sold.alex@gmail.com>
2021-04-15 14:39:35 +0200

    Dry version with OR

--- Rock_paper_scissors/rock_paper_scissor.py
+++ Rock_paper_scissors/rock_paper_scissor.py
@@ -2,14 +2,9 @@ import random
 pc_choice = random.choice(["scissors","rock","paper"])
 
 user_choice = input("What you choose - rock,paper or scissors?\n")
-if pc_choice == user_choise:
+if pc_choice == user_choice:
     print("Repeat Again")
-elif user_choice == "rock" and pc_choice == "scissors":
+elif user_choice == "rock" and pc_choice == "scissors" or user_choice == "paper" and pc_choice == "rock" or user_choice == "scissors" and pc_choice == "paper":
     print("You Win")
-elif user_choice == "paper" and pc_choice == "rock":
-    print("You Win")
-elif user_choice == "scissors" and pc_choice == "paper":
-    print("You Win")
-
 else:
     print("You loose :( Pc Wins")
