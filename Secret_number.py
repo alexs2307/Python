c5e9506e323152c80c5b71b4fd9ce74379163718
alexs2307 <sold.alex@gmail.com>
2021-04-18 21:35:23 +0200

    added randint and while loop

--- Secret_number.py
+++ Secret_number.py
@@ -1,7 +1,14 @@
-secret = str(5)
-guess = input("Guess the secret number between 1 and 10 ")
-
-if secret == guess:
-    print("Congratulations the secret number is 5!! ")
-else:
-    print("Oh no!! Better luck next time")
+from random import randint
+secret_number = randint(1, 30)
+guess = 0
+while guess != secret_number:
+    number = input("Guess the secret number \n")
+    guess = int(number)
+    if guess == secret_number:
+        print("You win")
+    else:
+        if guess > secret_number:
+            print("Too high try something smaller")
+        else:
+            print("Too low try something bigger")
+print("Game over")
