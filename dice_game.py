318ed6e55e115303f4b0715be8c7ae594724756d
Alex Scher <alexs@actual.local>
2021-04-22 22:53:19 +0200

    dice_game_with_functions

---
+++ dice_game.py
@@ -0,0 +1,25 @@
+import random
+
+
+def roll_dice():
+    dice_total = random.randint(1, 6) + random.randint(1, 6)
+    return dice_total
+def main():
+    player1 = input("Player 1 name\n")
+    print(player1.upper())
+    player2 = input("Player 2 name\n")
+    print(player2.upper())
+
+    roll1 = roll_dice()
+    roll2 = roll_dice()
+
+    print(player1.upper(), "rolled", roll1)
+    print(player2.upper(), "rolled", roll2)
+
+    if roll1 > roll2:
+        print(player1.upper(), "wins")
+    elif roll1 == roll2:
+        print("Draw")
+    else:
+        print(player2.upper(), "wins")
+main()
